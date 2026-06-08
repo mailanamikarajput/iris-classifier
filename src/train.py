49c0d9356a7267ce02658dbb6317420fa70fd5ab
mailanamikarajput <mailanamikarajput@gmail.com>
2026-06-08 22:24:46 +0100

    My final commit

---
+++ src/train.py
@@ -0,0 +1,64 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+from pathlib import Path
+from joblib import dump
+
+# Build your first AI Model
+
+iris = load_iris()
+
+# Storing features in x and target in Y
+
+x = iris.data
+y = iris.target
+
+# Divide Training and Test data set
+
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.2, random_state=42)
+
+#Train the model
+
+model = DecisionTreeClassifier(random_state=42)
+trained_model=model.fit(x_train,y_train)
+
+
+# Predict test data using newly trained model
+
+y_pred=model.predict(x_test)
+ 
+# Model accuracy check
+
+accuracy=accuracy_score(y_pred,y_test)
+print("Accuracy:",accuracy)
+
+# Calculate confussion matrix
+
+conf =confusion_matrix(y_pred,y_test)
+
+print("Confusion Matrix:",conf)
+
+
+# Compute confusion matrix
+cm = confusion_matrix(y_pred,y_test)
+# Plot
+disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+disp.plot(cmap="Blues")
+
+output_dir = Path.cwd()
+
+# save the model 
+dump(model, output_dir.parent/"outputs/model.joblib")
+
+# Save as PNG
+plt.savefig(output_dir.parent/"outputs/confusion_matrix.png", dpi=300, bbox_inches="tight")
+
+# Optional: display on screen
+plt.show()
+
+# Optional: free memory
+plt.close()
---
+++ tests/readme.md
@@ -0,0 +1,12 @@
+# Iris classifier (Decision Tree)
+
+## Overview
+End to End ML example from digital marketig mastery modeule -> build a decision tree classifier on the classic iris dataset using scikitlearn.
+
+## Quick Start
+'''bash
+git clone https://github.com/mailanamikarajput/iris-classifier/
+cd iris_classifier
+python -m venv venv && source venv/bin/activate
+pip install -r requirements.txt
+python scr/train.py
